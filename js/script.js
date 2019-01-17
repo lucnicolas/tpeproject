480ea3fde84f4efa0bff2a7a1aaea4f74fa1a2a3
lucnicolas <l.nicolas@intech-sud.fr>
2019-01-17 14:47:15 +0100

    ajout fonctions jquery hide() show() + .innerHTML des totaux

---
+++ js/script.js
@@ -0,0 +1,151 @@
+/* --- VARIABLES --- */
+//Déclaraction des variables totals
+let total_calories=0
+let total_lipides=0
+let total_glucides=0
+let total_proteines=0
+//Déclaration du dataMapping
+let dataMapping = {
+    matin: {
+        "option1": {
+            calories: "250",
+            lipides: "20",
+            glucides: "14",
+            proteines: "33"
+        },
+        "option2": {
+            calories: "800",
+            lipides: "20",
+            glucides: "14",
+            proteines: "33"
+        },
+        "option3": {
+            calories: "280",
+            lipides: "20",
+            glucides: "14",
+            proteines: "33"
+        },
+        "option4": {
+            calories: "555",
+            lipides: "20",
+            glucides: "14",
+            proteines: "33"
+        }
+    },
+    midi: {
+        "option5": {
+            calories: "332",
+            lipides: "20",
+            glucides: "14",
+            proteines: "33"
+        },
+        "option6": {
+            calories: "101",
+            lipides: "20",
+            glucides: "14",
+            proteines: "33"
+        },
+        "option7": {
+            calories: "902",
+            lipides: "20",
+            glucides: "14",
+            proteines: "33"
+        },
+        "option8": {
+            calories: "654",
+            lipides: "20",
+            glucides: "14",
+            proteines: "33"
+        }
+    },
+    soir: {
+        "option9": {
+            calories: "403",
+            lipides: "20",
+            glucides: "14",
+            proteines: "33"
+        },
+        "option10": {
+            calories: "298",
+            lipides: "20",
+            glucides: "14",
+            proteines: "33"
+        },
+        "option11": {
+            calories: "330",
+            lipides: "20",
+            glucides: "14",
+            proteines: "33"
+        },
+        "option12": {
+            calories: "509",
+            lipides: "20",
+            glucides: "14",
+            proteines: "33"
+        }
+    }
+};
+
+
+
+/* --- FONCTIONS --- */
+    
+function valeur_matin() {
+    select = document.getElementById("matin");
+    choice = select.selectedIndex;
+    valeur = select.options[choice].value;
+    let data = dataMapping.matin[select.options[choice].value];
+    total_calories += parseFloat(data.calories);
+    total_lipides += parseFloat(data.lipides);
+    total_glucides += parseFloat(data.glucides);
+    total_proteines += parseFloat(data.proteines);
+}
+
+
+function valeur_midi() {
+    select = document.getElementById("midi");
+    choice = select.selectedIndex;
+    valeur = select.options[choice].value;
+    let data = dataMapping.midi[select.options[choice].value];
+    total_calories += parseFloat(data.calories);
+    total_lipides += parseFloat(data.lipides);
+    total_glucides += parseFloat(data.glucides);
+    total_proteines += parseFloat(data.proteines);
+}
+
+
+function valeur_soir() {
+    select = document.getElementById("soir");
+    choice = select.selectedIndex;
+    valeur = select.options[choice].value;
+    let data = dataMapping.soir[select.options[choice].value];
+    total_calories += parseFloat(data.calories);
+    total_lipides += parseFloat(data.lipides);
+    total_glucides += parseFloat(data.glucides);
+    total_proteines += parseFloat(data.proteines);
+    document.getElementById('cal').innerHTML = total_calories;
+    document.getElementById('lip').innerHTML = total_lipides;
+    document.getElementById('glu').innerHTML = total_glucides;
+    document.getElementById('pro').innerHTML = total_proteines;
+}
+
+
+
+//Fenête d'alert avec les données finales
+function affichage_totaux(){
+    alert("Calories : "+total_calories+"\nLipides : "+total_lipides+"\nGlucides : "+total_glucides+"\nProteines : "+total_proteines);
+    console.log("Tout va bien :)") 
+}
+
+
+
+
+$(document).ready(function() {
+
+    $( "#button1" ).click(function() {
+        $( "#block3" ).show( "slow" );
+        $( "#button1" ).hide( "slow" );
+    });
+        
+});
+
