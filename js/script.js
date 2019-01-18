f34f7113f9f1a76ca8fc37afbdf088341012f3c4
lucnicolas <l.nicolas@intech-sud.fr>
2019-01-18 19:15:52 +0100

    correction bug modal intro + déplacement des scripts présent dans l'index vers le fichier js

--- js/script.js
+++ js/script.js
@@ -165,10 +165,28 @@ $(document).ready(function() {$
         $( "#loading" ).hide( "slow" );
     }); 
 
+    // Get the modal
+let modal = document.getElementById('description');
+
+// Get the button that opens the modal
+let btn = document.getElementById("button1");
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
     // When the user clicks on <span> (x), close the modal
-    //$( ".close" ).click(function() {
-     //   $( ".modal" ).fadeOut( 400 );
-    //});   
+    $( ".close" ).click(function() {
+        $( ".modal" ).fadeOut( 400 );
+    });   
+    
     $("#button1").hover(function(){
         if (checkForm===true){
         $(this).css("background", "black");
